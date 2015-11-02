a3874f3398f374b6265a1010ea17bb0857cf45ea
gorkaguerrero <gorka666@gmail.com>
2015-11-02 18:12:47 +0000

    completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,115 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  suma = 0
+  
+  for i in (0..arr.count-1)
+    suma += arr[i]  
+  end
+  return suma
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  suma = 0
+  arr.sort! {|x, y| y <=> x}
+  
+  if arr.count == 0
+    return suma
+  elsif arr.count == 1
+    return arr[0]
+  else 
+    return arr[0]+arr[1]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  suma = false
+
+  if arr.count == 0
+    return false
+  elsif arr.count == 1
+    return false
+  else 
+    for i in (0..arr.count-2)
+      for j in (i+1..arr.count-1)
+        if (arr[i]+arr[j] == n)
+          return true
+        end
+      end
+    end
+    return false
+    
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  m = s.capitalize
+  if s.length == 0
+    return false
+  end
+  
+  if 'BCDFGHJKLMNPQRSTVWXYZ'.include?(m[0])
+    return true
+  else
+    return false
+  end
+  
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length == 0
+    return false
+  end
+  
+  if s =~ /^[01]*$/
+    n = s.to_i(2)
+    return (n%4 == 0)
+    
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.length != 0
+      @isbn = isbn
+    else
+      raise ArgumentError.new("'#{isbn}' is not valid")
+    end
+    if price > 0
+      @price = price
+    else
+      raise ArgumentError.new("'#{price}' is not valid")
+    end    
+  end
+  
+  def price= (p)
+    @price = p
+  end
+  
+  def isbn= (i)
+    @isbn = i
+  end
+  
+  def price
+    return @price
+  end
+  
+  def isbn
+    return @isbn
+  end
+  
+  def price_as_string
+    return "$#{format("%.2f", @price)}"
+  end
+  
 end
